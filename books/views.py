aed3f03052850db5e09bbd7dbe6cf1d1660a4896
JuliaAndreyeva <blackonblackua@gmail.com>
2024-05-31 13:04:56 +0300

    added book details

--- books/views.py
+++ books/views.py
@@ -7,3 +7,7 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'book_detail.html', {'book': book})
