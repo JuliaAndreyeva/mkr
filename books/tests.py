d42bbad09c8118aa0911b6d10606e10048a61b65
JuliaAndreyeva <blackonblackua@gmail.com>
2024-05-31 13:04:10 +0300

    added some test

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import Book, Author
+from django.urls import reverse
 
-# Create your tests here.
+
+class AuthorViewTests(TestCase):
+
+    def setUp(self):
+        self.author1 = Author.objects.create(name="Author One", bio="Bio for author one")
+        self.author2 = Author.objects.create(name="Author Two", bio="Bio for author two")
+        Book.objects.create(title="Book One", description="Description for book one", publication_date="2023-01-01", price=10.00)
+        Book.objects.create(title="Book Two", description="Description for book two", publication_date="2023-01-02", price=15.00)
+
+
+    def test_author_list_view(self):
+        response = self.client.get(reverse('author_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.author1.name)
+        self.assertContains(response, self.author2.name)
+
+
+    def test_author_detail_view(self):
+        response = self.client.get(reverse('author_detail', args=[self.author1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.author1.name)
+        self.assertContains(response, self.author1.bio)
+
+
+class BookViewTests(TestCase):
+
+    def setUp(self):
+        self.author1 = Author.objects.create(name="Author One", bio="Bio for author one")
+        self.book1 = Book.objects.create(title="Book One", description="Description for book one", publication_date="2023-01-01", price=10.00)
+        self.book2 = Book.objects.create(title="Book Two", description="Description for book two", publication_date="2023-01-02", price=15.00)
+        self.book1.authors.add(self.author1)
+        self.book2.authors.add(self.author1)
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.book1.title)
+        self.assertContains(response, self.book2.title)
+
+    def test_book_detail_view(self):
+        response = self.client.get(reverse('book_detail', args=[self.book1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.book1.title)
+        self.assertContains(response, self.book1.description)
+
--- books/views.py
+++ books/views.py
@@ -7,18 +7,3 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
-
-def book_detail(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    return render(request, 'book_detail.html', {'book': book})
-
-
-def author_list(request):
-    authors = Author.objects.annotate(book_count=Count('book'))
-    return render(request, 'author_list.html', {'authors': authors})
-
-
-def author_detail(request, author_id):
-    author = get_object_or_404(Author, pk=author_id)
-    return render(request, 'author_detail.html', {'author': author})
-
